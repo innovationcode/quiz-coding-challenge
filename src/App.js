86deef2eeb6ae6ae19bfa8c2c2cb1abd2c7b66ba
lajawanti <lajawanti.dhake@gmail.com>
2020-10-29 16:00:38 -0400

    questionData.js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import { data } from './questionData/questionData.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Quiz-Coding-Challenge..</h1>
+      <h1>Quiz&nbsp; Coding&nbsp; Challenge.</h1>
     </div>
   );
 }
